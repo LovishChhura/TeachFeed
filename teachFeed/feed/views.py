56bce814f26c7d32a376aece67d1b4d0f15568d1
Lovish Chhura <vishaledu786@gmail.com>
2022-03-23 21:35:02 +0530

    added starter template and extended

--- teachFeed/feed/views.py
+++ teachFeed/feed/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def feed(request):
-    return HttpResponse("This is feed")
+    return render(request,"feed/feed.html")
