f827d4a09e3e14222de68738106c9471b24f1cf5
Lovish Chhura <vishaledu786@gmail.com>
2022-03-23 21:19:23 +0530

    added templates, static, apps in settings, edited urls.py

--- teachFeed/feed/views.py
+++ teachFeed/feed/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def feed(request):
+    return HttpResponse("This is feed")
--- teachFeed/home/views.py
+++ teachFeed/home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home/home.html')
+
+def login(request):
+    return HttpResponse('You are logged In')
+
+def extra(request):
+    return HttpResponse('This is extra')
