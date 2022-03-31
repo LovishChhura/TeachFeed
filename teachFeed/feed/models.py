da2d015f8bb85bb933d83dbe7ad139e05f69e130
Lovish Chhura <vishaledu786@gmail.com>
2022-03-31 16:13:44 +0530

    added tables to database

--- teachFeed/feed/models.py
+++ teachFeed/feed/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+class Department(models.Model):
+    sno = models.AutoField(primary_key=True)
+    department = models.CharField(
+            max_length=100,
+            help_text='Enter a Department (e.g. IT)',
+            validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        return self.department
+
+class Teacher(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(
+        max_length=200,
+        help_text='Enter a name of Teacher',
+        validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+    )
+    department = models.ForeignKey('Department', on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(
+        max_length=200,
+        help_text='Enter a name of Subject (e.g. DSA)',
+        validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+    )
+    teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True, help_text='Teacher teaching this subject')
+    department = models.ForeignKey('Department', on_delete=models.CASCADE, null=False, help_text='Department under which this subject comes')
+
+    def __str__(self):
+        return self.name
--- teachFeed/home/models.py
+++ teachFeed/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+class Access(models.Model):
+    sno = models.AutoField(primary_key=True)
+    department = models.CharField(
+            max_length=100,
+            help_text='Enter a Department (e.g. IT)',
+            validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+    )
+    accesscd = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.department
