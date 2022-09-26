31c25860c0c765dc89d08b2c4f69ebf24bbb92fc
Lovish Chhura <vishaledu786@gmail.com>
2022-09-26 21:47:44 +0530

    added for hereku

--- teachFeed/teachFeed/urls.py
+++ teachFeed/teachFeed/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
 
 admin.site.site_header="teachFeed Admin"
 admin.site.site_title="teachFeed Admin Panel"
